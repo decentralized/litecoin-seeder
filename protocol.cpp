cf028c10bd3699620787c9cb703278732e7f67d7
p1r0 <p1r0@nethunters.xyz>
2021-10-02 00:42:32 +0000

    Fixed fork to work with Maxcoin

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb };
+unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xbb, 0xd2 };
 
 CMessageHeader::CMessageHeader()
 {
